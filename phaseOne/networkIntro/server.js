30e597cdd9192675492632d7f57bf5491aad3ff7
rahique <rahiqueosman@gmail.com>
2025-06-28 03:54:34 -0400

    update

--- phaseOne/networkIntro/server.js
+++ phaseOne/networkIntro/server.js
@@ -74,7 +74,7 @@ const server = http.createServer((req, res) => {
 `);
 });
 
-const PORT = 2505;
+const PORT = 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
